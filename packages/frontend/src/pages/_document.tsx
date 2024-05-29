e9438b7e5651f9d72a8cf47f16f54c4a09692716
Matias Poblete <matias.pobleteduran@gmail.com>
2024-05-29 18:21:37 -0400

    🔥 Removed non existent references

--- packages/frontend/src/pages/_document.tsx
+++ packages/frontend/src/pages/_document.tsx
@@ -40,9 +40,7 @@ export default class MyDocument extends Document<NewDocumentInitialProps> {
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-config" content="/browserconfig.xml" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#7e5da8" />
           <meta name="msapplication-TileColor" content="#7e5da8" />
           <meta name="theme-color" content="#000000" />
