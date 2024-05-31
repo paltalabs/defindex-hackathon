de0f12379f643ab42660caa1e8596e68cb77328f
coderipper <hello@joaquinsoza.dev>
2024-05-31 04:15:48 -0500

    deposit working

--- packages/frontend/src/components/DepositToIndex/DepositToIndex.tsx
+++ packages/frontend/src/components/DepositToIndex/DepositToIndex.tsx
@@ -1,21 +1,42 @@
+import { DefindexMethod, useDefindexCallback } from '@/hooks/useDefindex'
 import { Button, Card, Input } from '@chakra-ui/react'
-import React from 'react'
+import { useSorobanReact } from '@soroban-react/core'
+import { Address, nativeToScVal, xdr } from '@stellar/stellar-sdk'
+import React, { useState } from 'react'
 
 function DepositToIndex() {
-  const defaultDefindex = 'CCKW6SMINDG6TUWJROIZ535EW2ZUJQEDGSKNIK3FBK26PAMBZDVK2BZA'
-  const handleDeposit = () => {
-    console.log('Deposit')
+  const [defindex_address, set_defindex_address] = useState<string>(String)
+  const [amount, set_amount] = useState<number>(0)
+
+  const {address} = useSorobanReact();
+  const defindex = useDefindexCallback()
+
+  const depositDefindex = async () => {
+    if (!address || !amount) return;
+
+    const depositParams: xdr.ScVal[] = [
+      nativeToScVal((amount * Math.pow(10, 7)), { type: "i128" }),
+      new Address(address).toScVal()
+    ];
+
+    console.log('deploying Defindex')
+    const result = await defindex(
+      DefindexMethod.DEPOSIT,
+      defindex_address,
+      depositParams,
+      true,
+    )
+    console.log('🚀 ~ deployDefindex ~ result:', result);
+    return result;
   }
-  const handleInput = (e: any) => {
-    console.log(e.target.value)
-  }
+
   return (
     <>
       <h2>DepositToIndex</h2>
       <Card variant="outline" px={16} py={16} bgColor="whiteAlpha.100">
-        <Input my={4} type="text" onChange={handleInput} placeholder='Defindex address' value={defaultDefindex} />
-        <Input my={4} type="text" onChange={handleInput} placeholder='Amount' value={13} />
-        <Button my={4} colorScheme='green' onClick={handleDeposit}>Deposit</Button>
+        <Input my={4} type="text" onChange={(e) => set_defindex_address(e.target.value)} placeholder='Defindex address' value={defindex_address} />
+        <Input my={4} type="text" onChange={(e) => set_amount(Number(e.target.value))} placeholder='Amount' value={amount} />
+        <Button my={4} colorScheme='green' onClick={depositDefindex}>Deposit</Button>
       </Card>
     </>
   )
